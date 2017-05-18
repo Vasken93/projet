27475526ba2b0d9aa44da9c49ebd620722a92250
huantar <isn.dudek.tomasz@gmail.com>
2017-05-18 11:41:17 +0200

    Add files via upload

---
+++ Cavalier.java
@@ -0,0 +1,17 @@
+package projetSynthese;
+
+
+public class Cavalier extends Piece {
+	
+	
+	public Cavalier(String Couleur) {
+		super("Cavalier", Couleur);
+	}
+
+	
+	public boolean estValide(Deplacement deplacement) {
+		
+		return (Math.abs(deplacement.getDeplacementX() / deplacement.getDeplacementY())) == 2 |
+				(Math.abs(deplacement.getDeplacementX() / deplacement.getDeplacementY())) == .5;
+	}
+}
